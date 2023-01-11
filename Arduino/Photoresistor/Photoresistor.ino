60668ef0369a37926549565881958fca98fd7177
Guilherme Pedroso <82010727+guiipedroso@users.noreply.github.com>
2023-01-10 22:19:52 -0300

    Photoresistor

---
+++ Arduino/Photoresistor/Photoresistor.ino
@@ -0,0 +1,32 @@
+/*
+  Title: Photoresistor
+  Author: Guilherme Pedroso de Lima
+  Date: Jan 2023
+  Updated: --
+*/ 
+
+const int pot = A2;
+int led = 13;
+int variable = 0;
+int blink;
+
+void 
+setup() 
+  {
+  pinMode(pot, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);  
+  }
+
+void
+loop()
+  {
+  variable = analogRead(pot);
+  Serial.println(variable);
+  digitalWrite(led, HIGH);
+  blink = variable/10;
+  delay(blink);
+  digitalWrite(led, LOW);
+  blink = variable/10;
+  delay(blink);  
+  }
